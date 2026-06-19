c6bcdaa2b25ade2da30df0c51fc3b8fbf0e0a84a
Praveenkumar Kalidass <praveenkumar.kalidass@equalexperts.com>
2026-06-19 10:55:15 +0530

    feat: Add deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: 'rhine-order-management', // Set the base path to the repository name for GitHub Pages deployment
 };
 
 export default nextConfig;
